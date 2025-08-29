53d6a81b169228354a0f86f8b99ca11a3d4f5b77
Tony Troeff <tonyslav_troeff@outlook.com>
2025-08-29 09:58:10 +0300

    Solving problems from CodeForces - `Round 1044`.

---
+++ Codeforces/Round #1044/Redstone.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename... T>
+class optimized_unordered_map : public unordered_map<T...> {
+public:
+    template<typename... Args>
+    explicit optimized_unordered_map(Args &&... args) : unordered_map<T...>(forward<Args>(args)...) {
+        this->reserve(1024);
+        this->max_load_factor(0.25);
+    }
+};
+
+#define uint unsigned int
+#define ll long long
+#define ull unsigned long long
+#define MOD ((ll)(1e9 + 7))
+#define UNKNOWN (-1)
+
+// #define SINGLE_TEST
+
+void init() {
+}
+
+void solve() {
+    int n;
+    cin >> n;
+
+    optimized_unordered_map<int, int> freq;
+    for (int i = 0; i < n; i++) {
+        int current_num;
+        cin >> current_num;
+
+        freq[current_num]++;
+    }
+
+    const bool ok = ranges::any_of(freq, [](const pair<int, int> &el) { return el.second > 1; });
+    cout << (ok ? "YES" : "NO") << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    init();
+
+#ifdef SINGLE_TEST
+    int t = 1;
+#else
+    int t;
+    cin >> t;
+#endif
+
+    while (t--)
+        solve();
+}
---
+++ Codeforces/Round #1044/Villagers.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename... T>
+class optimized_unordered_map : public unordered_map<T...> {
+public:
+    template<typename... Args>
+    explicit optimized_unordered_map(Args &&... args) : unordered_map<T...>(forward<Args>(args)...) {
+        this->reserve(1024);
+        this->max_load_factor(0.25);
+    }
+};
+
+#define uint unsigned int
+#define ll long long
+#define ull unsigned long long
+#define MOD ((ll)(1e9 + 7))
+#define UNKNOWN (-1)
+
+// #define SINGLE_TEST
+
+void init() {
+}
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> g(n);
+    for (int &el: g) cin >> el;
+
+    ranges::sort(g);
+
+    ll ans = 0;
+    for (int i = n - 1; i >= 0; i -= 2) ans += g[i];
+
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    init();
+
+#ifdef SINGLE_TEST
+    int t = 1;
+#else
+    int t;
+    cin >> t;
+#endif
+
+    while (t--)
+        solve();
+}
