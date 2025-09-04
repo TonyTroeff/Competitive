4e6842d466db8b33c81b782ab13259ceab51fea0
Tony Troeff <tonyslav_troeff@outlook.com>
2025-09-04 21:29:46 +0300

    Solving problems from CodeForces - `Round 1045`.

---
+++ Codeforces/Round #1045/Add 0 or K.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename... T>
+class optimized_unordered_map : public unordered_map<T...> {
+public:
+    template<typename... Args>
+    explicit optimized_unordered_map(Args &&... args) : unordered_map<T...>(forward<Args>(args)...) {
+        this->reserve(1024);
+        this->max_load_factor(0.25);
+    }
+};
+
+#define uint unsigned int
+#define ll long long
+#define ull unsigned long long
+#define MOD ((ll)(1e9 + 7))
+#define UNKNOWN (-1)
+
+// #define SINGLE_TEST
+
+void init() {
+}
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<ll> a(n);
+    for (ll &el: a) cin >> el;
+
+    for (int i = 0; i < n; i++)
+        cout << a[i] + k * (a[i] % (k + 1)) << ' ';
+    cout << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    init();
+
+#ifdef SINGLE_TEST
+    int t = 1;
+#else
+    int t;
+    cin >> t;
+#endif
+
+    while (t--)
+        solve();
+}
